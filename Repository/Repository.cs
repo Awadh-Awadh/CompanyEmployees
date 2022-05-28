b44d37c2443082605292585a9853fdd12301d679
Awadh Awadh <awadhawadh84@yahoo.com>
2022-05-28 21:56:19 +0300

    add repository implementation

---
+++ Repository/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using Infra.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext;
+
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+                RepositoryContext = repositoryContext;
+        }
+    }
+}
