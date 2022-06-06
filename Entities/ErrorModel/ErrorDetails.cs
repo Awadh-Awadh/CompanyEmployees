fab76dcb847de23d77f98c41239f4f0e470a13fa
Awadh Awadh <awadhawadh84@yahoo.com>
2022-06-06 23:39:28 +0300

    add method to get all employees in a given company

--- Entities/ErrorModel/ErrorDetails.cs
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Entities.ErrorModel
 {
-    internal class ErrorDetails
+    // chapter 5
+    public class ErrorDetails
     {
+        public string StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+       
     }
 }
