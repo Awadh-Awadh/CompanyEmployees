7350e80148e60026b6dfb618c17bfca49b49c324
Awadh Awadh <awadhawadh84@yahoo.com>
2022-06-07 22:00:17 +0300

    add endpoint to get all employees in an organization

--- CompanyEmployees/Controllers/EmployeeController.cs
+++ CompanyEmployees/Controllers/EmployeeController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
+
     public class EmployeeController : ControllerBase
     {
+        private readonly IRepositoryManager _repository;
+        private readonly ILogger _logger;
+        public EmployeeController(IRepositoryManager repositoryManager, ILogger logger)
+        {
+            repositoryManager = _repository;
+            logger = _logger;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAllEmployee(int companyId, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompanyById(companyId, trackChanges: false);
+            if (company == null)
+            {
+                _logger.LogError("No company found");
+                return NotFound();  
+            }
+
+            var employees = _repository.Employee.GetAllEmployees(company.Id , trackChanges: false);
+            return Ok(employees.Select(x => new EmployeeDTO
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Position = x.Position,
+                Age = x.Age
+            }));
+        }
     }
+
+    
 }
--- Entities/DTOs/EmployeeDTO.cs
+++ Entities/DTOs/EmployeeDTO.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Entities.DTOs
 {
-    internal class EmployeeDTO
+    public class EmployeeDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
     }
 }
